c1d7661a3ab753fa33f6dcbe2b1c9d49b8630857
Jacob Hinze <kontakt@jacobhinze.dk>
2018-09-17 16:03:07 +0200

    Added main class

---
+++ src/main/java/com/shadow5353/McBan.java
@@ -0,0 +1,18 @@
+package com.shadow5353;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Jacob on 17-09-2018.
+ */
+public class McBan extends JavaPlugin {
+
+    @Override public void onEnable() {
+        saveDefaultConfig();
+    }
+
+    public static Plugin getPlugin() {
+        return Bukkit.getServer().getPluginManager().getPlugin("McBan");
+    }
+}
